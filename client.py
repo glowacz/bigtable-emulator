6788b8e3db00fa3d87f5bef9145f32764897d87b
glowacz <glowacki.pj@gmail.com>
2026-02-05 23:08:10 +0100

    feat: python client
    maybe will be useful
    created it, because it was supposed to be able to delete cfs from specific rows

---
+++ client.py
@@ -0,0 +1,20 @@
+# This is the only way to test DeleteFromFamily, there is no cbt command that would do this
+
+# pip3 install google-cloud-bigtable
+# But first, run in a virtual environment:
+# (once) python -m venv venv
+# source venv/bin/activate
+
+# this script also needs
+# export BIGTABLE_EMULATOR_HOST=localhost:8888
+from google.cloud import bigtable
+
+client = bigtable.Client(project="p", admin=True)
+instance = client.instance("i")
+table = instance.table("cars")
+row = table.row("car-1")
+
+# Deletes all cells in the 'history' column family for this row
+row.delete_cells(column_family_id="params", columns=row.ALL_COLUMNS)
+# row.delete()
+row.commit()
